8c4e26db06d22efc353baa090ae8b9a89ab3e4f2
Irek Iwanca <iiwanca@gmail.com>
2021-11-20 07:34:43 +0100

    init

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+  //Pierwszym parametrem poniżej w 'res.render' jest nazwa szablonu czyli 'index.pug' w folderze 'views', który wysyłamy. Drugi to obiekt '{ title: 'Express' }', w którym przekazuje wszystkie dodatkowe parametry przekazywane do naszego szablonu.
   res.render('index', { title: 'Express' });
 });
 
